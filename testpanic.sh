bcee59b0036637d75cdc423e3edc2982f415ef9c
nick10mc <60902354+nick10mc@users.noreply.github.com>
2024-08-31 23:14:02 -0400

    Update testpanic.sh
    Added a confirmation checkpoint as well as checks for the presence of the Watchdog timer lines in system.conf.

--- testpanic.sh
+++ testpanic.sh
@@ -7,35 +7,78 @@
 
 ##### WARNING, this script should ONLY be used to test the watchdog timer
 # Requires permission to run
+W="\033[1;37m" # White
+R="\033[1;31m" # Red
+
+
+echo -e "\n
+\n${R}******************************
+\n${W}WATCHDOG TESTER - KERNEL PANIC
+\n******************************
+\nUsage: Induces a kernel panic, used for testing the Watchdog settings
+\n${R}WARNING: This script will crash the kernel! Save and close all documents prior to proceeding!
+\n**DELETE THIS FILE AFTER CONFIRMING WDT OPERATION**
+"                                                                                                                
 confFile="/etc/systemd/system.conf"
 
-# Check if watchdog timer is enabled
-if ! grep -q "^RuntimeWatchdogSec=" "$confFile"
-then
-    echo "RuntimeWatchdogSec not set!"
-    sleep 5
-    exit
-fi
+## Ask user if they are sure about roasting their kernels
+echo -e "\n${W}Are you sure you want to crash the system? \nY/N?"
+read -r CONFIRM
 
-## Now, check the Reboot Watchdog
-if ! grep -q "^RebootWatchdogSec=" "$confFile"
+if [ "$CONFIRM" != "Y" ] && [ "$CONFIRM" != "y" ]
 then
-    echo "RebootWatchdogSec not set!"
-    sleep 5
-    exit
-fi
+    echo -e "\n${W}Kernel Panic Aborted"
+    
+else
+    echo -e "\n${R}Kernel Panic Confirmed..."
+    # Set confFile location
+    confFile="/etc/systemd/system.conf"
 
-# Run the following commands as root
-sudo su - <<EOF
-whoami
-# Lets make some popcorn
-sudo echo 1 > /proc/sys/kernel/sysrq
-sudo echo c > /proc/sysrq-trigger
-whoami
+    # Set countdown time to kernel panic
+    countdown=10
+
+    # Loop until the countdown reaches zero, then fire up the microwave
+    while [ $countdown -gt 0 ]; do
+        # -ne clears the terminal line when the cursor is \r returned to the beginning of the line
+        echo -ne "${W}Time remaining to Panic: ${R}$countdown seconds\r"
+    
+        # Decrement the countdown
+        ((countdown--))
+    
+        # Wait for 1 second
+        sleep 1
+    done
+
+    # Check if watchdog timer is enabled
+    if ! grep -q "^RuntimeWatchdogSec=" "$confFile"
+    then
+        echo "RuntimeWatchdogSec not set!"
+        sleep 5
+        exit
+    fi
+
+    ## Now, check the Reboot Watchdog
+    if ! grep -q "^RebootWatchdogSec=" "$confFile"
+    then
+        echo "RebootWatchdogSec not set!"
+        sleep 5
+        exit
+    fi
+
+    # Run the following commands as root
+    sudo su - <<'EOF'
+    whoami
+    # Lets make some popcorn
+    sudo echo 1 > /proc/sys/kernel/sysrq
+    sudo echo c > /proc/sysrq-trigger
+    whoami
 
 EOF
 
+    # Print a final message
+    echo -e "\nKernels roasted...\nIf you can read this, something is wrong with the set permission levels!"
+
+fi
+
 sleep 5
 exit
-
-
